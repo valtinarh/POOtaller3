4f42292af4519f57964a6c8eecb747e3e00e263c
Valentina Rodriguez Henao <144085605+valtinarh@users.noreply.github.com>
2023-09-19 21:41:46 -0500

    Ejercicio 7

---
+++ poo7/valorA.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codigo18.poo7;
+
+/**
+ *
+ * @author varoh
+ */
+public class valorA {
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+       
+    }
+    
+}
