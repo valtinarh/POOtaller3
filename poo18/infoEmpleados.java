2188c780f4eb4e80566ada7863ba9ce539344a5c
Valentina Rodriguez Henao <144085605+valtinarh@users.noreply.github.com>
2023-09-19 21:44:14 -0500

    Ejercicio 18

---
+++ poo18/infoEmpleados.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codigo18.poo18;
+
+/**
+ *
+ * @author varoh
+ */
+public class infoEmpleados {
+    public static void main (String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+        
+    }
+}
