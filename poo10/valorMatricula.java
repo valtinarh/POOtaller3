b866101b7f9577645a53b50fd2dca7de29e48ec1
Valentina Rodriguez Henao <144085605+valtinarh@users.noreply.github.com>
2023-09-19 21:42:41 -0500

    Ejercicio 10

---
+++ poo10/valorMatricula.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codigo18.poo10;
+
+/**
+ *
+ * @author varoh
+ */
+public class valorMatricula {
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
