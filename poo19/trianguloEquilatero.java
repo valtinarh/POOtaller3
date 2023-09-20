ea9172afbee910f4e018baa086e34ae3e44ca4e6
Valentina Rodriguez Henao <144085605+valtinarh@users.noreply.github.com>
2023-09-19 21:44:50 -0500

    Ejercicio 19

---
+++ poo19/trianguloEquilatero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codigo18.poo19;
+
+/**
+ *
+ * @author varoh
+ */
+public class trianguloEquilatero {
+    public static void main (String[] args) {
+        form form = new form();
+        form.setVisible(true);
+    }
+}
